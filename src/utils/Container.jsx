87761c17d7e12b816db8c4fae584aeaca5361477
= <tubajan282@gmail.com>
2026-07-30 04:31:09 +0500

    Home Page FrontEnd

--- src/utils/Container.jsx
+++ src/utils/Container.jsx
@@ -1,6 +1,7 @@
 const Container = ({children}) => {
     return (
-        <div className="w-full min-w-[300px] max-w-[1550px] mx-auto">
+        <div className="w-full min-w-[300px] max-w-[100%] mx-auto">
+        {/* <div className="w-full min-w-[300px] max-w-[1550px] mx-auto"> */}
             
             {children}
         </div>
